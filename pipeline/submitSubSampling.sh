04a6e0ca0e8b3e55d2d1785da7b8ae98a24126d5
Nicolas Delhomme <nicolas.delhomme@plantphys.umu.se>
2019-11-28 12:05:55 +0100

    fixed the path for the out and err file

--- pipeline/submitSubSampling.sh
+++ pipeline/submitSubSampling.sh
@@ -3,7 +3,8 @@
 set -eux
 
 proj=snic2019-8-310
-mail=jenna.lihavainen@umu.se
+#mail=jenna.lihavainen@umu.se
+mail=nicolas.delhomme@slu.se
 MREADS=20000000
 
 in=/proj/snic2019-30-28/senescence-RNA-Seq/results/trimmomatic
@@ -15,6 +16,7 @@ fi
 
 for f in $(find $in -name "P12255_1*_trimmomatic_[1,2].fq.gz"); 
 do
-  sbatch -A $proj -p core -n 1 -e $out/${f//.fq.gz/.err} \
-  -o $out/${f//.fq.gz/.out} -t 1:00:00 runSubSampling.sh $f $MREADS $out
+  fnam=$(basename ${f//.fq.gz/})
+  sbatch -A $proj -p core -n 1 -e $out/$fnam.err \
+  -o $out/$fnam.out -t 1:00:00 runSubSampling.sh $f $MREADS $out
 done
