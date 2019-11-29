89f094053ed9dc3076663670f5acbf5ba8bd3086
Jenna Lihavainen <jlihavainen@aspseq.fysbot.umu.se>
2019-11-29 15:55:06 +0100

    Corrected a typo, missing /

--- pipeline/submitSalmon_subsamples.sh
+++ pipeline/submitSalmon_subsamples.sh
@@ -7,7 +7,7 @@ proj=snic2019-8-324
 mail=jenna.lihavainen@umu.se
 
 ## process the argument (just change in and out)
-in=proj/snic2019-30-28/senescence-RNA-Seq/results/trimmomatic/subsample 
+in=/proj/snic2019-30-28/senescence-RNA-Seq/results/trimmomatic/subsample 
 ref=/proj/uppoff2019006/indices/Potra02/Potra02_v2dot2_transcripts_salmon-v14dot1.inx
 out=/proj/snic2019-30-28/senescence-RNA-Seq/results/salmon/subsample
 bind=/proj/uppoff2019006:/proj/uppoff2019006
@@ -20,7 +20,7 @@ fi
 
 ## for every file
 for f in $(find $in -name "*sortmerna_trimmomatic_1_sub_20000000.fq.gz"); do
-  fnam=$(basename ${f/_1_sub_20000000.fq.gz/})
+  fnam=$(basename ${f/_1.fq.gz/})
 
   ## execute
   sbatch -A $proj --mail-user=$mail \
