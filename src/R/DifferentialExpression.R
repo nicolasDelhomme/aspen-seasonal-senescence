523588fcaf69fda30cc54c73273388a643d6031f
Jenna Lihavainen <jlihavainen@aspseq.fysbot.umu.se>
2022-01-12 14:21:58 +0100

    DE method

--- src/R/DifferentialExpression.R
+++ src/R/DifferentialExpression.R
@@ -1,6 +1,6 @@
 #' ---
 #' title: "Differential Expression"
-#' author: "Nicolas Delhomme & Domenique Andre"
+#' author: "Nicolas Delhomme & Jenna Lihavainen"
 #' date: "`r Sys.Date()`"
 #' output:
 #'  html_document:
@@ -21,8 +21,11 @@ suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(VennDiagram))
 
 #' * Helper files
-suppressMessages(source("~/Git/UPSCb/src/R/plotMA.R"))
-suppressMessages(source("~/Git/UPSCb/src/R/volcanoPlot.R"))
+suppressMessages({
+    source(here("UPSCb-common/src/R/volcanoPlot.R"))
+    source(here("UPSCb-common/src/R/featureSelection.R"))
+})
+
 
 #' * Graphics
 pal=brewer.pal(8,"Dark2")
@@ -38,7 +41,7 @@ mar <- par("mar")
     return(
         ggplot(bind_cols(as.data.frame(colData(dds)),
                          melt(vst[sel,])),
-               aes(x=Time,y=value,col=Experiment,group=Experiment)) +
+               aes(x=Time,y=value,col=Genotype,group=Genotype)) +
             geom_point() + geom_smooth() +
             scale_y_continuous(name="VST expression") + 
             ggtitle(label=paste("Expression for: ",gene_id))
@@ -93,30 +96,30 @@ mar <- par("mar")
 
 #' # _Populus tremula_
 #' * Data
-load(here("../../analysis/salmon/Potra-104-127-139-removed-dds.rda"))
+load(here("data/analysis/salmon/dds_subsampling_no_outliers.rda"))
 
 #' ## Normalisation for visualisation
 vsd <- varianceStabilizingTransformation(dds,blind=FALSE)
 vst <- assay(vsd)
 vst <- vst - min(vst)
+write_tsv(as.data.frame(vst_gen1) %>% rownames_to_column("ID"),here("data/analysis/salmon/variance-stabilised_model-aware_data.tsv"))
+
 
 #' ## Gene of interest
-#' * Potra000962g07909
 #' The gene is not expressed
-line_plot(dds,vst,"Potra000962g07909")
+line_plot(dds,vst,"Potra2n4c10046")
 
-#' * Potra001661g13641
-#' The gene is  medium expressed and show time-lagged expression patterns between
-#' the experiments
-line_plot(dds,vst,"Potra001661g13641")
+#' The gene expression levels differ between the genotypes
+line_plot(dds,vst,"Potra2n19c33539")
 
-#' * Potra003711g22520
-#' The gene is medium expressed and show initially opposing expression patterns between experiments
-line_plot(dds,vst,"Potra003711g22520")
+#' The gene displays enhanced expression during autumn
+line_plot(dds,vst,"Potra2n15c28290")
 
-#' * Potra006413g25676
-#' The gene his very lowly expressed and shows somewhat opposite expression patterns
-line_plot(dds,vst,"Potra006413g25676")
+#' The gene displays repressed expression during autumn
+line_plot(dds,vst,"Potra2n2c4558")
+
+#' The gene displays different temporal expression pattern between the genotypes
+line_plot(dds,vst,"Potra2n14c27101")
 
 #' ## Differential Expression
 dds <- DESeq(dds)
@@ -125,63 +128,168 @@ dds <- DESeq(dds)
 #' The dispersion estimation is adequate
 plotDispEsts(dds)
 
-#' The model used is:
-#' 
-#' `Experiment * Time` meaning that the `Experiment` and `Time variable` as 
-#' well as their interaction `Experiment:Time` is considered. Because we 
-#' cannot assume that these two variables explain all the variance in the data,
-#' there is also an `Intercept` for the linear model. This also implies that the 
-#' model assumes `Cont` at `3` hours to be the baseline; _i.e._ everything is compared 
-#' against it.
-resultsNames(dds)
+#' #' The model used is:
+#' #' `Genotype` and `Time`. Time effect is considered within whole data and within individual genotypes. 
 
-#' ## Results
-#' In the following we look at the interaction specific genes; _i.e._ genes that 
-#' changes at a given time transition in between experiments
-#' ### ECM _vs._ Cont at T3
-Pa_3 <- extract_results(dds,vst,"Experiment_ECM_vs_Cont",
-                        default_prefix="Potra_ECM-vs-Cont_T3_",
-                        labels=paste0(colData(dds)$Experiment,
-                                      colData(dds)$Time),
-                        sample_sel=colData(dds)$Time==3)
+ddsTime <- dds
+design(ddsTime) = ~ Time
+ddsTime$Time <- relevel(ddsTime$Time,"225")
+ddsTime <- DESeq(ddsTime)
+resultsNames(ddsTime)
 
-#' ### ECM _vs._ Cont at T7
-Pa_7 <- extract_results(dds,vst,c(0,1,0,0,0,0,1,0,0,0),
-                        default_prefix="Potra_ECM-vs-Cont_T7_",
-                        labels=paste0(colData(dds)$Experiment,
-                                      colData(dds)$Time),
-                        sample_sel=colData(dds)$Time==7)
+# time effect 264 vs 225 DOY (Senescence-associated genes)
+dir.create(here("../../../../Git/aspen-seasonal-senescence/data/analysis/DE/"), showWarnings = FALSE)
 
-#' ### ECM _vs._ Cont at T14
-Pa_14 <- extract_results(dds,vst,c(0,1,0,0,0,0,0,1,0,0),
-                         default_prefix="Potra_ECM-vs-Cont_T14_",
-                         labels=paste0(colData(dds)$Experiment,
-                                       colData(dds)$Time),
-                         sample_sel=colData(dds)$Time==14)
+T264vs225 <- extract_results(ddsTime,vst,"Time_264_vs_225",
+                             default_dir = here("data/analysis/DE/Time"),
+                             default_prefix = "T264-225_",
+                             labels=paste(ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("225","264"),
+                             plot = TRUE,verbose = TRUE)
 
-#' ### ECM _vs._ Cont at T21
-Pa_21 <- extract_results(dds,vst,c(0,1,0,0,0,0,0,0,1,0),
-                         default_prefix="Potra_ECM-vs-Cont_T21_",
-                         labels=paste0(colData(dds)$Experiment,
-                                       colData(dds)$Time),
-                         sample_sel=colData(dds)$Time==21)
+# time effects (consecutive time points)
+dir.create(here("data/analysis/DE/"),showWarnings = FALSE)
+T232vs225 <- extract_results(ddsTime,vst,"Time_232_vs_225",
+                             default_dir = here("data/analysis/DE/Time"),
+                             default_prefix = "T232-225_",
+                             labels=paste(ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("225","232"),
+                             plot = TRUE,verbose = TRUE)
+    
+T237vs232 <- extract_results(ddsTime,vst,list("Time_237_vs_225","Time_232_vs_225"),
+                             default_dir = here("data/analysis/DE/Time"),
+                             default_prefix = "T237-232_",
+                             labels=paste(ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("232","237"),
+                             plot = TRUE,verbose = TRUE)
 
-#' ### ECM _vs._ Cont at T28
-Pa_28 <- extract_results(dds,vst,c(0,1,0,0,0,0,0,0,0,1),
-                         default_prefix="Potra_ECM-vs-Cont_T28_",
-                         labels=paste0(colData(dds)$Experiment,
-                                       colData(dds)$Time),
-                         sample_sel=colData(dds)$Time==28)
+T241vs237 <- extract_results(ddsTime,vst,list("Time_241_vs_225","Time_237_vs_225"),
+                             default_dir = here("data/analysis/DE"),
+                             default_prefix = "T241-237_",
+                             labels=paste(ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("241","237"),
+                             plot = TRUE,verbose = TRUE)
 
-#' ### Venn Diagram
-grid.newpage()
-grid.draw(venn.diagram(list(T3=Pa_3,
-                            T7=Pa_7,
-                            T14=Pa_14,
-                            T21=Pa_21,
-                            T28=Pa_28),
-                       NULL,
-                       fill=pal[1:5]))
+T246vs241 <- extract_results(ddsTime,vst,list("Time_246_vs_225","Time_241_vs_225"),
+                             default_dir = here("data/analysis/DE/Time"),
+                             default_prefix = "T246-241_",
+                             labels=paste(ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("246","241"),
+                             plot = TRUE,verbose = TRUE)
+
+T250vs246 <- extract_results(ddsTime,vst,list("Time_250_vs_225","Time_246_vs_225"),
+                             default_dir = here("data/analysis/DE/Time"),
+                             default_prefix = "T250-246_",
+                             labels=paste(dds$Genotype,ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("250","246"),
+                             plot = TRUE,verbose = TRUE)
+
+T253vs250 <- extract_results(ddsTime,vst,list("Time_253_vs_225","Time_250_vs_225"),
+                             default_dir = here("data/analysis/DE/Time"),
+                             default_prefix = "T253-250_",
+                             labels=paste(dds$Genotype,ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("253","250"),
+                             plot = TRUE,verbose = TRUE)
+
+T256vs253 <- extract_results(ddsTime,vst,list("Time_256_vs_225","Time_253_vs_225"),
+                             default_dir = here("data/analysis/DE/Time"),
+                             default_prefix = "T256-253_",
+                             labels=paste(ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("256","253"),
+                             plot = TRUE,verbose = TRUE)
+
+T264vs256 <- extract_results(ddsTime,vst,list("Time_264_vs_225","Time_256_vs_225"),
+                             default_dir = here("data/analysis/DE/Time"),
+                             default_prefix = "T264-256_",
+                             labels=paste(ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("264","256"),
+                             plot = TRUE,verbose = TRUE)
+
+# Genotype effect 
+
+ddsGenotype <- dds
+design(ddsGenotype) = ~ Genotype
+ddsGenotype$Genotype <- relevel(ddsGenotype$Genotype,"1")
+ddsGenotype <- DESeq(ddsGenotype)
+
+Gen1vs48 <- extract_results(ddsGenotype,vst,"Genotype_48_vs_1",
+                             default_dir = here("data/analysis/DE/Genotype"),
+                             default_prefix = "Gen1-gen48_",
+                             labels=paste(dds$Genotype,sep="_"),
+                             sample_sel = ddsGenotype$Genotype %in% c("48","1"),
+                             plot = TRUE,verbose = TRUE)
+
+Gen1vs81 <- extract_results(ddsGenotype,vst,"Genotype_81_vs_1",
+                               default_dir = here("data/analysis/DE/Genotype"),
+                               default_prefix = "Gen1-gen81_",
+                               labels=paste(dds$Genotype,sep="_"),
+                               sample_sel = ddsGenotype$Genotype %in% c("81","1"),
+                               plot = TRUE,verbose = TRUE)
+
+Gen48vs81 <- extract_results(ddsGenotype,vst,list("Genotype_48_vs_1","Genotype_81_vs_1"),
+                             default_dir = here("data/analysis/DE/Genotype"),
+                             default_prefix = "Gen48-gen81_",
+                             labels=paste(dds$Genotype,sep="_"),
+                             sample_sel = ddsGenotype$Genotype %in% c("81","48"),
+                             plot = TRUE,verbose = TRUE)
+
+
+#Time effect in genotype 1
+
+dds_gen1 <- dds[, dds$Genotype %in% c("1")]
+dds_gen1$Genotype <- droplevels(dds_gen1$Genotype)
+design(dds_gen1) = ~ Time
+dds_gen1$Time <- relevel(dds_gen1$Time,"225")
+dds_gen1 <- DESeq(dds_gen1)
+
+# time effect 264 vs 225 (Senescence associated genes-SAGs)
+
+T264vs225 <- extract_results(dds_gen1,vst,"Time_264_vs_225",
+                             default_dir = here("data/analysis/DE/Time/Gen1"),
+                             default_prefix = "T264-225_",
+                             sample_sel = dds_gen1$Time %in% c("225","264"),
+                             plot = TRUE,verbose = TRUE)
+
+# time effect (compared to 225)
+
+T256vs225 <- extract_results(dds_gen1,vst,"Time_256_vs_225",
+                             default_dir = here("data/analysis/DE/Time/Gen1"),
+                             default_prefix = "T256-225_",
+                             sample_sel = ddsTime$Time %in% c("225","256"),
+                             plot = TRUE,verbose = TRUE)
+
+T253vs225 <- extract_results(dds_gen1,vst,"Time_253_vs_225",
+                             default_dir = here("data/analysis/DE/Time/Gen1"),
+                             default_prefix = "T253-225_",
+                             sample_sel = ddsTime$Time %in% c("225","253"),
+                             plot = TRUE,verbose = TRUE)
+
+T250vs225 <- extract_results(dds_gen1,vst,"Time_250_vs_225",
+                             default_dir = here("data/analysis/DE/Time/Gen1"),
+                             default_prefix = "T250-225_",
+                             sample_sel = ddsTime$Time %in% c("225","250"),
+                             plot = TRUE,verbose = TRUE)
+
+
+T246vs225 <- extract_results(dds_gen1,vst,"Time_246_vs_225",
+                             default_dir = here("data/analysis/DE/Time/Gen1"),
+                             default_prefix = "T246-225_",
+                             sample_sel = ddsTime$Time %in% c("225","246"),
+                             plot = TRUE,verbose = TRUE)
+
+T241vs225 <- extract_results(dds_gen1,vst,"Time_241_vs_225",
+                             default_dir = here("data/analysis/DE/Time/Gen1"),
+                             default_prefix = "T241-225_",
+                             sample_sel = ddsTime$Time %in% c("225","241"),
+                             plot = TRUE,verbose = TRUE)
+
+T237vs225 <- extract_results(dds_gen1,vst,"Time_237_vs_225",
+                             default_dir = here("data/analysis/DE/Time/Gen1"),
+                             default_prefix = "T237-225_",
+                             labels=paste(dds$Genotype,ddsTime$Time,sep="_"),
+                             sample_sel = ddsTime$Time %in% c("225","237"),
+                             plot = TRUE,verbose = TRUE)
+
 
 #' # Session Info 
 #'  ```{r session info, echo=FALSE}
